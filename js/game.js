967893e2bcd6b3bb95e563c29c81b6853b46e5f3
kampholtangruanrat-boop <kampholtangruanrat@gmail.com>
2026-08-07 19:30:02 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -20,24 +20,63 @@ function renderParty() {
 
     const partyArea = document.getElementById("party");
 
-    partyArea.innerHTML = "";
+partyArea.innerHTML += `
 
-    gameState.party.forEach(character => {
+<div>
 
-        partyArea.innerHTML += `
-            <div>
-                <h3>${character.name}</h3>
-                <p>
-                Class: ${character.class}<br>
-                HP: ${character.hp}/${character.max_hp}<br>
-                AC: ${character.ac}
-                </p>
-            </div>
-        `;
+<h3>${character.name}</h3>
+
+<p>
+Class: ${character.class}<br>
+HP: ${character.hp}/${character.max_hp}<br>
+AC: ${character.ac}
+</p>
+
+<button onclick="setActiveCharacter('${character.id}')">
+Select
+</button>
+
+</div>
+
+<hr>
+
+`;
 
     });
 
 }
+function setActiveCharacter(id) {
 
+    gameState.activeCharacter =
+        gameState.party.find(
+            character => character.id === id
+        );
+
+    renderActiveCharacter();
+
+}
+
+
+function renderActiveCharacter() {
+
+    const activeArea =
+        document.getElementById("active");
+
+    const character =
+        gameState.activeCharacter;
+
+
+    activeArea.innerHTML = `
+
+        <h2>Active Character</h2>
+
+        <h3>${character.name}</h3>
+
+        Class: ${character.class}<br>
+        HP: ${character.hp}/${character.max_hp}<br>
+        AC: ${character.ac}
+
+    `;
+}
 
 loadCharacters();
