238717c2c9f6a9b0532e54efaf4b0ddebb0da2a1
kampholtangruanrat-boop <kampholtangruanrat@gmail.com>
2026-08-09 00:20:22 +0700

    Update movement.js

--- js/movement.js
+++ js/movement.js
@@ -29,9 +29,19 @@ function canMoveTo(character,x,y,map){
     }
 
 
-    character.movement.remaining -= cost;
-
-
     return true;
+}
+
+
+function getMovementCost(character,x,y,map){
+
+    const distance =
+        Math.max(
+            Math.abs(character.position.x - x),
+            Math.abs(character.position.y - y)
+        );
+
+
+    return distance * map.rules.feetPerSquare;
 
 }
