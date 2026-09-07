61a932fb325fa09857c7ce5381c73892e2e9311b
kampholtangruanrat-boop <kampholtangruanrat@gmail.com>
2026-09-07 14:14:22 +0700

    Step 1: use centralized movement resolver

--- js/game.js
+++ js/game.js
@@ -375,7 +375,6 @@ function moveActiveCharacter(x,y){
     character.position.y = y;
 
 
-
     character.movement.remaining -= result.cost;
 
 
@@ -404,89 +403,6 @@ function moveActiveCharacter(x,y){
 
 
 }
-
-
-
-
-
-function canMoveTo(character,x,y,map){
-
-
-    const remaining =
-        character.movement.remaining;
-
-
-
-    const distance =
-
-        Math.max(
-
-            Math.abs(
-                character.position.x - x
-            ),
-
-            Math.abs(
-                character.position.y - y
-            )
-
-        );
-
-
-
-    const cost =
-
-        distance *
-        map.rules.feetPerSquare;
-
-
-
-
-
-    if(cost > remaining){
-
-
-        console.log(
-
-            "Too far",
-
-            cost,
-
-            "/",
-
-            remaining
-
-        );
-
-
-        return {
-
-            allowed:false,
-
-            cost:0
-
-        };
-
-
-    }
-
-
-
-
-
-    return {
-
-
-        allowed:true,
-
-        cost:cost
-
-
-    };
-
-
-}
-
-
 
 
 
